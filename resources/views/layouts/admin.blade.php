35c023dc2b3d7d825e7e2ab57fe0d36b6d296dd2
IvanAlx641 <ivan76321@gmail.com>
2026-02-05 18:03:22 -0600

    prueba

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -227,12 +227,7 @@
                                         </a>
                                     </li>
 
-                                    <li class="sidebar-item">
-                                        <a href="#" class="sidebar-link">
-                                            <i class="ti ti-file-search"></i>
-                                            <span class="hide-menu">Buscador</span>
-                                        </a>
-                                    </li>
+                                    
                                 </ul>
                             </li>
 
